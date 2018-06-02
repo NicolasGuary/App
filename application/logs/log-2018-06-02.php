06d515db1303e6c8c31b2d82d4e6e055b341ab67
Nicolas Guary <nicolas.guary@etu.umontpellier.fr>
2018-06-02 22:24:42 +0200

    back

---
+++ application/logs/log-2018-06-02.php
@@ -0,0 +1,9 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2018-06-02 20:14:18 --> Query error: No database selected - Invalid query: SELECT token FROM userTokens WHERE idUser = '19'
+ERROR - 2018-06-02 20:14:21 --> Query error: No database selected - Invalid query: SELECT token FROM userTokens WHERE idUser = '19'
+ERROR - 2018-06-02 20:14:41 --> Query error: No database selected - Invalid query: SELECT token FROM userTokens WHERE idUser = '19'
+ERROR - 2018-06-02 20:16:47 --> Query error: No database selected - Invalid query: SELECT token FROM userTokens WHERE idUser = '19'
+ERROR - 2018-06-02 20:16:49 --> Query error: No database selected - Invalid query: SELECT token FROM userTokens WHERE idUser = '19'
+ERROR - 2018-06-02 20:19:02 --> Query error: No database selected - Invalid query: SELECT token FROM userTokens WHERE idUser = '19'
+ERROR - 2018-06-02 20:19:13 --> Query error: No database selected - Invalid query: SELECT token FROM userTokens WHERE idUser = '19'
