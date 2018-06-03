bc1f55e168bd4329fcae9bf8d3ec724d9a8419de
Nicolas Guary <nicolas.guary@etu.umontpellier.fr>
2018-06-03 13:04:32 +0200

    active likes only

--- application/models/PostModel.php
+++ application/models/PostModel.php
@@ -162,7 +162,7 @@
             $query = $this->db->query(
                 'SELECT likers.idLiker, user.nom, user.prenom, post.id, post.titre
                 FROM likers, user, post 
-                WHERE likers.idPost = post.id and likers.idLiker = user.id and likers.idLiker = ?
+                WHERE likers.idPost = post.id and likers.idLiker = user.id and likers.idLiker = ? and likers.state = 1
                 ORDER BY likers.id DESC
                 LIMIT 5',$idUser);
             return $query->result_array();
